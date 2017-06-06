97f768dbd723ed02b0ffe5470cd1b1b6118a64b1
splasher <splasher@Splash-aaa.local>
2017-06-06 15:56:26 +0700

    update

--- sites/all/themes/blogger_theme/js/global.js
+++ sites/all/themes/blogger_theme/js/global.js
@@ -1,25 +1,33 @@
-(function($) {
-	"use strict";
-	$(document).ready(function(){
-		// Main menu superfish
-		$('#main-menu > ul').superfish({
-			delay: 200,
-			animation: {opacity:'show', height:'show'},
-			speed: 'fast',
-			cssArrows: false,
-			disableHI: true
-		});
-		
-		// Mobile Menu
-		$('#navigation-toggle').sidr({
-			name: 'sidr-main',
-			source: '#sidr-close, #site-navigation',
-			side: 'left',
-			displace: false
-		});
-		$(".sidr-class-toggle-sidr-close").click( function() {
-			$.sidr('close', 'sidr-main');
-			return false;
-		});
-	}); // End doc ready
+(function ($) {
+  "use strict";
+  $(document).ready(function () {
+    // Main menu superfish
+    $('#main-menu > ul').superfish({
+      delay: 200,
+      animation: {opacity: 'show', height: 'show'},
+      speed: 'fast',
+      cssArrows: false,
+      disableHI: true
+    });
+
+    // Mobile Menu
+    $('#navigation-toggle').sidr({
+      name: 'sidr-main',
+      source: '#sidr-close, #site-navigation',
+      side: 'left',
+      displace: false
+    });
+    $(".sidr-class-toggle-sidr-close").click(function () {
+      $.sidr('close', 'sidr-main');
+      return false;
+    });
+
+    wow = new WOW(
+            {
+              animateClass: 'animated',
+              offset: 100
+            }
+    );
+    wow.init();
+  }); // End doc ready
 })(jQuery);
