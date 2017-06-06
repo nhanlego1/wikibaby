b9e3a5fec7a7bf97468d4620ebf230cf66116071
splasher <splasher@Splash-aaa.local>
2017-06-06 13:11:54 +0700

    update

--- sites/all/themes/blogger_theme/templates/html.tpl.php
+++ sites/all/themes/blogger_theme/templates/html.tpl.php
@@ -8,7 +8,7 @@
 <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 <script>
   (adsbygoogle = window.adsbygoogle || []).push({
-    google_ad_client: "ca-pub-3568208598414242",
+    google_ad_client: "ca-pub-4843590392513296",
     enable_page_level_ads: true
   });
 </script>
