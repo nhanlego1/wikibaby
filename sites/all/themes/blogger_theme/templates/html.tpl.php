b33144c5e791e81c57f1a39cdb81d3d394767bdc
splasher <splasher@Splash-aaa.local>
2017-06-06 11:33:16 +0700

    update core

--- sites/all/themes/blogger_theme/templates/html.tpl.php
+++ sites/all/themes/blogger_theme/templates/html.tpl.php
@@ -5,6 +5,13 @@
 <title><?php print $head_title; ?></title>
 <?php print $styles; ?>
 <?php print $scripts; ?>
+<script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script>
+  (adsbygoogle = window.adsbygoogle || []).push({
+    google_ad_client: "ca-pub-3568208598414242",
+    enable_page_level_ads: true
+  });
+</script>
 <!--[if lt IE 9]><script src="<?php print base_path() . drupal_get_path('theme', 'blogger_theme') . '/js/html5.js'; ?>"></script><![endif]-->
 </head>
 <body class="<?php print $classes; ?>"<?php print $attributes; ?>>
